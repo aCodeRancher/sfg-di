542200d2b2f5ef06c350fe81cc37007eaf01bc2c
helen <hhma@alumni.cpp.edu>
2021-05-11 23:06:57 -0400

    remove unnecessary imports.

--- src/test/java/guru/springframework/sfgdi/PropertySourceTest.java
+++ src/test/java/guru/springframework/sfgdi/PropertySourceTest.java
@@ -1,21 +1,14 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.config.MyConfiguration;
-import guru.springframework.sfgdi.config.TestConfiguration;
 import guru.springframework.sfgdi.datasource.FakeDataSource;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 @TestPropertySource(locations="classpath:application-test.yml")
 @SpringBootTest
